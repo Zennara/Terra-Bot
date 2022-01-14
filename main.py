304920a0a9ddc909b84f95607f6c3d47d8871284
Keagan Landfried <64995253+Zennara@users.noreply.github.com>
2022-01-14 07:40:39 +0000

    rate limit checker

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import discord
 import os
 import keep_alive
+from replit import db
+import requests
 
 bot = discord.Bot()
 
 guild_ids = [806706495466766366]
 
+#api limit checker
+r = requests.head(url="https://discord.com/api/v1")
+try:
+  print(f"Rate limit {int(r.headers['Retry-After']) / 60} minutes left")
+except:
+  print("No rate limit")
+
 @bot.event
 async def on_ready():
-    print(f"We have logged in as {bot.user}")
+    print(f"{bot.user.name} Online.")
 
 @bot.slash_command(guild_ids=guild_ids)
-async def hello(ctx):
+async def hello(ctx, opt):
     await ctx.respond("Hello!")
 
   
